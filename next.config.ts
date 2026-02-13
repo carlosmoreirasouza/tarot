8c634a5187e76dfd6ae5cb7b23d6c8d26983d184
Carlos Souza <carlos.henrike13@gmail.com>
2026-02-13 13:56:21 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental:{
+    allowedDevOrigins:[
+      "http://76.13.171.33:3000",
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+    ],
+  },
 };
 
 export default nextConfig;
